a3b6c7d0708d714e4fa6892539c4322aa8f6b145
anpandu <anpandumail@gmail.com>
2018-04-27 14:11:43 +0700

    postgresql util

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 import re
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config(object):
 
@@ -7,3 +10,9 @@ class Config(object):
   ROWS_NUM = int(os.getenv('ROWS_NUM', '1'))
   METADATA = os.getenv('METADATA', './examples/metadata.json')
   LOCALE = os.getenv('LOCALE', 'en_US')
+
+  PG_HOST = os.getenv('PG_HOST', 'localhost')
+  PG_PORT = os.getenv('PG_PORT', '5432')
+  PG_USER = os.getenv('PG_USER', 'admin')
+  PG_PASSWORD = os.getenv('PG_PASSWORD', 'admin123')
+  PG_DATABASE = os.getenv('PG_DATABASE', 'mydb')
